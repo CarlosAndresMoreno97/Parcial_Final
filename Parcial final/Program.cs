cc0c1f8eb3abe4654b03e82cde93576fca6a4736
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2024-05-31 21:46:11 -0500

    desarrollo del pacial final

---
+++ Parcial final/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int[,] board;
+    static int score;
+    static int playerRow;
+    static int playerCol;
+    static int size;
+
+    static void Main(string[] args)
+    {
+        InitializeGame();
+        while (true)
+        {
+            DisplayBoard();
+            DisplayScore();
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            HandleKey(keyInfo.Key);
+        }
+    }
