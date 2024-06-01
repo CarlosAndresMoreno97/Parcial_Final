484a6143054d3a018d73c73bbfe3a4c73404f15a
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2024-05-31 21:53:57 -0500

    contianuacin del ejercicio

--- Parcial final/Program.cs
+++ Parcial final/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-class Program
+public class Program
 {
     static int[,] board;
     static int score;
@@ -10,6 +10,8 @@ class Program
 
     static void Main(string[] args)
     {
+
+
         InitializeGame();
         while (true)
         {
@@ -17,5 +19,44 @@ class Program
             DisplayScore();
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
             HandleKey(keyInfo.Key);
+        }
+    }
+    static void InitializeGame()
+    {
+        Console.WriteLine("Ingrese el tamaño del tablero (N): ");
+        size = Convert.ToInt32(Console.ReadLine());
+        board = new int[size, size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                board[i, j] = rand.Next(1, 9);
+            }
+        }
+        playerRow = 0;
+        playerCol = 0;
+        board[playerRow, playerCol] = 0;
+        score = 0;
+    }
+    static void DisplayBoard()
+    {
+        Console.Clear();
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (i == playerRow && j == playerCol)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("[P] ");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write($"[{board[i, j]}] ");
+                }
+            }
+            Console.WriteLine();
         }
     }
